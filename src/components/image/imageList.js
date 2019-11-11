bd4da0751b144daa827800ac62b91a3320619a97
berkalparslan <34027499+berkalparslan@users.noreply.github.com>
2019-11-11 23:59:01 +0300

    key

--- src/components/image/imageList.js
+++ src/components/image/imageList.js
@@ -4,11 +4,11 @@ import './imageList.css'
 const ImageList = ({images}) => {
     const preparedImageList = images.map(image=>{
         return(
-            <img alt={image.id} src={image.urls.small} />
+            <img alt={image.id} key={image.id}  src={image.urls.small} />
         )
     })
     return(
-        
+
         <div className='image-list-container'>
             <images>{preparedImageList}</images>
         </div>
